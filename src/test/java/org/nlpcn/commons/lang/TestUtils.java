c6eb5344ca2d432b72bf8b771f967743ebfa96d6
zhl <zhl@home1.cn>
2015-07-16 03:25:09 +0800

    add testutils

---
+++ src/test/java/org/nlpcn/commons/lang/TestUtils.java
@@ -0,0 +1,13 @@
+package org.nlpcn.commons.lang;
+
+public class TestUtils {
+
+    public static String mainResources(final String file) {
+        //System.getenv().forEach((k, v) -> System.out.println(k));
+        //System.getProperties().forEach((k,v) -> System.out.println(k));
+        //System.out.println(System.getProperties().getProperty("user.dir"));
+        return System.getProperties().getProperty("user.dir")
+                + "/src/main/resources"
+                + (file.startsWith("/") ? file : "/" + file);
+    }
+}
