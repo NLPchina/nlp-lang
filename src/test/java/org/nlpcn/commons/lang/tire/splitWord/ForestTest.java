3148652e3af2abb78157b8a0f49a87bd1fd09c9c
ansj <ansj-sun@163.com>
2016-02-06 15:45:43 +0800

    update

--- src/test/java/org/nlpcn/commons/lang/tire/splitWord/ForestTest.java
+++ src/test/java/org/nlpcn/commons/lang/tire/splitWord/ForestTest.java
@@ -1,12 +1,8 @@
 package org.nlpcn.commons.lang.tire.splitWord;
 
-import java.io.BufferedReader;
-
 import org.junit.Test;
 import org.nlpcn.commons.lang.tire.GetWord;
 import org.nlpcn.commons.lang.tire.domain.Forest;
-import org.nlpcn.commons.lang.tire.library.Library;
-import org.nlpcn.commons.lang.util.IOUtil;
 
 public class ForestTest {
 
@@ -30,34 +26,6 @@ public class ForestTest {
 		while ((temp = word.getAllWords()) != null) {
 			System.out.println(temp);
 		}
-
-		Forest makeForest = Library.makeForest("D:\\git\\ansj_seg\\library\\default.dic");
-		
-		
-		GetWord word2 = makeForest.getWord("10,上城区小营街道大学路,余林,330102196204011513 ,2,13456808992,大学路新村44-122-102,大学路新村44-122-102,Z2015120110302017,Z,2015-12-25") ;
-		
-		while ((temp = word2.getAllWords()) != null) {
-			System.out.println(temp);
-		}
-		
-
-//		BufferedReader reader = IOUtil.getReader("D:\\temp\\News", "GBK");
-
-//		int j = 0;
-//
-//		String line = null;
-//
-//		while ((line = reader.readLine()) != null) {
-//
-//			word = makeForest.getWord(line);
-//
-//			while ((temp = word.getAllWords()) != null) {
-//				if (temp.indexOf('�') > 0)
-//					System.out.println(temp);
-//			}
-//		}
-//
-//		System.out.println(j);
 
 	}
 
