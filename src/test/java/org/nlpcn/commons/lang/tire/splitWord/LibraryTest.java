964c19d54498c033b7390145ed7023a2358ca92f
孙健 <sunj@infcn.com.cn>
2016-12-10 23:51:27 +0800

    fix travis err

--- src/test/java/org/nlpcn/commons/lang/tire/splitWord/LibraryTest.java
+++ src/test/java/org/nlpcn/commons/lang/tire/splitWord/LibraryTest.java
@@ -1,16 +1,18 @@
 package org.nlpcn.commons.lang.tire.splitWord;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.nlpcn.commons.lang.tire.domain.Forest;
 import org.nlpcn.commons.lang.tire.library.Library;
+
 
 public class LibraryTest {
 
 	@Test
 	public void test() throws Exception {
-		Forest makeForest = Library.makeForest("/Users/sunjian/Downloads/default.dic","gbk") ;
+		Forest makeForest = Library.makeForest("src/test/resources/library.txt","utf-8") ;
 		
-		System.out.println(makeForest.getBranch("上访"));
+		Assert.assertNotNull(makeForest.getBranch("上访"));
 	}
 
 }
